80be4e669bc405f22429e7570c6375c491f5f256
MyNameTsThad <pvpthadgaming@gmail.com>
2024-12-24 16:33:06 +0700

    basic functionality done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use core::error;
-use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}, str::FromStr};
+use std::path::PathBuf;
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream}
+    ,
+};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:1234").unwrap();
@@ -11,25 +16,40 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     println!("Accepted Connection!");
-    
+
     let buf = BufReader::new(&stream);
-    let request: Vec<_> = buf.lines()
+    let request: Vec<_> = buf
+        .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
     let request_path: Vec<&str> = request[0].split(" ").collect();
-    let path = format!(".{}", request_path[1]);
+    let path_string = format!("./html/{}", request_path[1]);
 
+    let mut path = PathBuf::from(path_string);
     let mut status = String::new();
-    let file_request = fs::read_to_string(path);
-    let file = match file_request {
-        Ok(content) => content,
-        Err(err) => panic!("{err:?}")
+
+    let content: String = if (path.exists()) {
+        if (path.is_dir()) {
+            path.push("index.html");
+        }
+        match fs::read_to_string(path) {
+            Ok(index) => {
+                status = "200 OK".to_string();
+                index
+            }
+            Err(err) => {
+                status = "404 Not Found".to_string();
+                fs::read_to_string("./html/404.html").unwrap()
+            }
+        }
+    } else {
+        status = "404 Not Found".to_string();
+        fs::read_to_string("./html/404.html").unwrap()
     };
-    println!("{}", file);
 
-    let template = "HTTP/1.1 200 OK\r\n\r\nhai!!!!!!!!!!!!!!!!!!!!";
+    let response = format!("HTTP/1.1 {}\r\n\r\n{}", status, content);
 
-    stream.write_all(template.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
 }
